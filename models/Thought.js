a9bd70ecad32d465d296a2b96cf470044abbc118
skybreakbb <willistaylora@gmail.com>
2023-08-22 18:30:45 -0500

    wrote more of User.js, started writing Thought.js

---
+++ models/Thought.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            min_length: 1,
+            max_length: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+    }
+)
+
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thought');
 
 const userSchema = new Schema(
     {
