bc55331852f04e92ad1c52a164332c9e95248c60
skybreakbb <willistaylora@gmail.com>
2023-08-22 18:27:18 -0500

    wrote majority of models/User.js

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            trimmed: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please enter a valid email address'],
+        },
+        thoughts: [thoughtSchema],
+        friends: [userSchema],
+    },
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
