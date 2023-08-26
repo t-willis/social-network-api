2a29d81fbcea56c96d81634f20b956e3ddade2be
skybreakbb <willistaylora@gmail.com>
2023-08-25 20:35:13 -0500

    wrote out more of models/Thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -13,8 +13,24 @@ const thoughtSchema = new Schema(
             type: Date,
             default: Date.now,
         },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
     }
 );
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
 
 const Thought = model('thought', thoughtSchema);
 
