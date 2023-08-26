1ed924358519ce3287fec013f9b4382b2287668f
skybreakbb <willistaylora@gmail.com>
2023-08-25 20:33:01 -0500

    updated models/User.js to allow virtuals

--- models/User.js
+++ models/User.js
@@ -28,6 +28,12 @@ const userSchema = new Schema(
             }
         ],
     },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
 );
 
 userSchema.virtual('friendCount').get(function () {
